2416388efdddb748bbacdb387f612ffe2ac2910f
Santiago Della Torre <sdellatorre@gmail.com>
2020-08-09 10:17:54 -0300

    Primer Commit

---
+++ src/edu/udc/pp/imperativo/Ejercicios.java
@@ -0,0 +1,18 @@
+package edu.udc.pp.imperativo;
+
+public class Ejercicios {
+	
+	public static void main(String[] args) {
+		
+		int n = 8;
+		
+		// Ejercicio A - Fibonacci
+		
+		
+		// Ejercicio B - Pascal 
+		
+		
+		
+	}
+
+}
